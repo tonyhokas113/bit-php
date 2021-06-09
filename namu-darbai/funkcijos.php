6bd9bcda8f9de29612f28ea3ac529ea57247ea1f
Vaigaudas <79445277+tonyhokas113@users.noreply.github.com>
2021-06-09 20:05:16 +0300

    func 10

--- namu-darbai/funkcijos.php
+++ namu-darbai/funkcijos.php
@@ -3,44 +3,44 @@
 
 ######################################### 1
 
-echo '<h3> Uzdavinys nr - 1 </h3>';
+// echo '<h3> Uzdavinys nr - 1 </h3>';
 
-function txt($text)
-{
-    return "<h1> $text </h1>";
-}
-echo txt('Labas rytas');
+// function txt($text)
+// {
+//     return "<h1> $text </h1>";
+// }
+// echo txt('Labas rytas');
 
-echo '<hr>';
+// echo '<hr>';
 
-######################################### 2
+// ######################################### 2
 
-echo '<h3> Uzdavinys nr - 2 </h3>';
+// echo '<h3> Uzdavinys nr - 2 </h3>';
 
-function tag($arg1, $arg2)
-{
-    return "<h$arg2> $arg1 </h$arg2>";
-}
-echo tag('Labas rytas', 4);
+// function tag($arg1, $arg2)
+// {
+//     return "<h$arg2> $arg1 </h$arg2>";
+// }
+// echo tag('Labas rytas', 4);
 
-echo '<hr>';
+// echo '<hr>';
 
-######################################### 3
+// ######################################### 3
 
-echo '<h3> Uzdavinys nr - 3 </h3>';
+// echo '<h3> Uzdavinys nr - 3 </h3>';
 
-$string = md5(time());
-echo "Random stringas: $string";
+// $string = md5(time());
+// echo "Random stringas: $string";
 
-preg_match_all('/\d{1,}/', $string, $nr);
+// preg_match_all('/\d{1,}/', $string, $nr);
 
-foreach ($nr[0] as $value) {
-    echo txt($value);
-}
+// foreach ($nr[0] as $value) {
+//     echo txt($value);
+// }
 
-echo '<hr>';
+// echo '<hr>';
 
-######################################### 4
+// ######################################### 4
 
 echo '<h3> Uzdavinys nr - 4 </h3>';
 
@@ -64,79 +64,79 @@ echo "Skaicius $number dalinasi -" . modulus($number) . ' kartus';
 
 echo '<hr>';
 
-######################################### 5
+// ######################################### 5
 
-echo '<h3> Uzdavinys nr - 5 </h3>';
+// echo '<h3> Uzdavinys nr - 5 </h3>';
 
-foreach (range(1, 100) as $key => $value) {
-    $array[] = rand(33, 77);
-}
+// foreach (range(1, 100) as $key => $value) {
+//     $array[] = rand(33, 77);
+// }
 
-foreach ($array as $key => $value) {
-    $array2[] = modulus($value) . ' - ' . $value;
-}
+// foreach ($array as $key => $value) {
+//     $array2[] = modulus($value) . ' - ' . $value;
+// }
 
-function sorted($a, $b)
-{
-    return - (substr($a, 0, 2) <=> substr($b, 0, 2));
-}
+// function sorted($a, $b)
+// {
+//     return - (substr($a, 0, 2) <=> substr($b, 0, 2));
+// }
 
-usort($array2, 'sorted');
+// usort($array2, 'sorted');
 
-_d($array2, '5 (Sorted)');
-print_r($array2);
+// _d($array2, '5 (Sorted)');
+// print_r($array2);
 
-echo '<hr>';
+// echo '<hr>';
 
-######################################### 6
+// ######################################### 6
 
-echo '<h3> Uzdavinys nr - 6 </h3>';
+// echo '<h3> Uzdavinys nr - 6 </h3>';
 
-foreach (range(1, 100) as $key => $value) {
-    $array3[] = rand(333, 777);
-}
+// foreach (range(1, 100) as $key => $value) {
+//     $array3[] = rand(333, 777);
+// }
 
-foreach ($array3 as $key => $value) {
-    if (modulus($value) == 0) {
-        $array4[] = $value;
-    }
-}
+// foreach ($array3 as $key => $value) {
+//     if (modulus($value) == 0) {
+//         $array4[] = $value;
+//     }
+// }
 
-_d($array3, '6');
-_d($array4, '6 (Prime numbers)');
-print_r($array4);
+// _d($array3, '6');
+// _d($array4, '6 (Prime numbers)');
+// print_r($array4);
 
-echo '<hr>';
+// echo '<hr>';
 
-######################################### 7
+// ######################################### 7
 
-echo '<h3> Uzdavinys nr - 7 </h3>';
+// echo '<h3> Uzdavinys nr - 7 </h3>';
 
-function generate($r)
-{
-    static $i = 1;
-    if ($i <= $r) {
-        $z = rand(10, 20);
-        foreach (range(1, $z) as $key => $_) {
-            if ($key == ($z - 1) && $i == $r) {
-                $arr[] = 0;
-            } elseif ($key == ($z - 1) && $i != $r) {
-                $i++;
-                $arr[] = generate($r);
-            } else {
-                $arr[] = rand(0, 10);
-            }
-        }
-        return $arr;
-    }
-}
+// function generate($r)
+// {
+//     static $i = 1;
+//     if ($i <= $r) {
+//         $z = rand(10, 20);
+//         foreach (range(1, $z) as $key => $_) {
+//             if ($key == ($z - 1) && $i == $r) {
+//                 $arr[] = 0;
+//             } elseif ($key == ($z - 1) && $i != $r) {
+//                 $i++;
+//                 $arr[] = generate($r);
+//             } else {
+//                 $arr[] = rand(0, 10);
+//             }
+//         }
+//         return $arr;
+//     }
+// }
 
-$r = rand(10, 30);
-$arejus = generate($r);
-_dc($arejus, '7');
-echo "Gylis - $r masyvai";
+// $r = rand(10, 30);
+// $arejus = generate($r);
+// _dc($arejus, '7');
+// echo "Gylis - $r masyvai";
 
-echo '<hr>';
+// echo '<hr>';
 
 //-----------------------------------------------------------
 
@@ -157,56 +157,55 @@ echo '<hr>';
 //         }
 //     return $masyvas;
 // }
-// _dc(doArray(rand(10, 30)), '7 uzduotis');
+// // _dc(doArray(rand(10, 30)), '7 uzduotis');
 
-######################################### 8
+// ######################################### 8
 
-function counter($arr)
-{
-    static $count = 0;
-    foreach ($arr as $value) {
-        if (!is_array($value)) {
-            $count += $value;
-        } else {
-            counter($value);
-        }
-    }
-    return $count;
-}
+// function counter($arr)
+// {
+//     static $count = 0;
+//     foreach ($arr as $value) {
+//         if (!is_array($value)) {
+//             $count += $value;
+//         } else {
+//             counter($value);
+//         }
+//     }
+//     return $count;
+// }
 
-_d(counter($arejus), '8');
+// _d(counter($arejus), '8');
 
-######################################### 9
+// ######################################### 9
 
 
-foreach (range(1, 3) as $key => $value) {
-    $masyvas[] = rand(1, 33);
-}
+// foreach (range(1, 3) as $_) {
+//     $masyvas[] = rand(1, 33);
+// }
 
-$run = 1;
-$from = 0;
-while ($run == 1) {
+// $run = 1;
+// $from = 0;
+// while ($run == 1) {
 
-    $prime = true;
-    $row = 0;
-    for ($i = $from; $i < count($masyvas); $i++) {
+//     $prime = true;
+//     $row = 0;
 
-        if (modulus($masyvas[$i]) == 0 && $masyvas[$i] != 1) {
-            $row++;
-        } else {
-            $prime = false;
-        }
-    }
-
-    if ($prime == false) {
-        $from++;
-        $masyvas[] = rand(1, 33);
-    }
-    if ($row == 3) {
-        $run = 0;
-    }
-}
-_d($masyvas, '9');
+//     for ($i = $from; $i < count($masyvas); $i++) {
+//         if (modulus($masyvas[$i]) == 0 && $masyvas[$i] != 1) {
+//             $row++;
+//         } else {
+//             $prime = false;
+//         }
+//     }
+//     if ($prime == false) {
+//         $from++;
+//         $masyvas[] = rand(1, 33);
+//     }
+//     if ($row == 3) {
+//         $run = 0;
+//     }
+// }
+// _d($masyvas, '9');
 
 //   2      3      5      7     11     13     17     19     23     29      31 
 
@@ -261,46 +260,40 @@ _d($masyvas, '9');
 
 echo '<h3> Uzdavinys nr - 10 </h3>';
 
-$suma = 0;
-$nariai = 0;
+$vidurkis = 1;
 foreach (range(1, 10) as $key => $value) {
     foreach (range(1, 10) as $key2 => $value2) {
         $arr2[$key2][] = rand(1, 100);
     }
 }
-
-$min = 10;
-$ind = 0;
-$d = 0;
-foreach ($arr2 as $key => $value) {
-    foreach ($value as $key2 => $value2) {
-        if (modulus($value2) == 0) {
-            $nariai++;
-            $suma += $value2;
+_d($arr2, '10');
+while (($vidurkis < 70)) {
+    $min = 100;
+    $suma = 0;
+    $nariai = 0;
+    foreach ($arr2 as $key => $value) {
+        foreach ($value as $key2 => $value2) {
+            if (modulus($value2) == 0) {
+                $nariai++;
+                $suma += $value2;
+            }
         }
     }
-}
-
-if ($suma / $nariai < 70) {
+    $vidurkis = $suma / $nariai;
     foreach ($arr2 as $key => $value) {
         foreach ($value as $key2 => $value2) {
             if ($value2 < $min) {
                 $min = $value2;
-                $ind = $key2;
+                $ind = $key;
+                $ind2 = $key2;
             }
         }
-        $d++;
     }
+    $arr2[$ind][$ind2] += 3;
 }
 
-echo $min;
-echo '<br>';
-echo $ind;
-echo '<br>';
-echo $d;
-echo '<br>';
-echo $suma / $nariai;
-_d($arr2, '10');
+echo 'Vidurkis po skaiciavimu: ' . round($vidurkis, 2);
+_d($arr2, '10 (Po skaiciavimo)');
 
 //-----------------------------------------------------------
 
