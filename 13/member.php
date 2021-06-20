3f44fee1842bb2d98285d7f33fb483d4161c7f14
Vaigaudas <79445277+tonyhokas113@users.noreply.github.com>
2021-06-20 12:17:06 +0300

    13 update

--- 13/member.php
+++ 13/member.php
@@ -3,22 +3,25 @@
 require __DIR__ . '/bootstrap.php';
 
 if (!isset($_SESSION['logged'])) {
-    header('Location: http://localhost/barsukas/13/login.php');
+    header('Location: http://127.0.0.1/bit-php/13/login.php');
     die;
 }
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Member Page</title>
 </head>
+
 <body>
     <?php include __DIR__ . '/menu.php' ?>
     <?php include __DIR__ . '/msg.php' ?>
     <h1>Member Page</h1>
 </body>
+
 </html>
